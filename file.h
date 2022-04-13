5693ffcf92fec46daa3f7e18551c03d8a130c796
Sidra <andrew.reeds.mpg@gmail.com>
2022-04-13 14:01:45 +0200

    Modified reading.

--- file.h
+++ file.h
@@ -5,25 +5,26 @@
 
 void init(FILE);
 
-static inline string readAllText(string path) {
-    FILE* f = fopen(cptr(path), "r");
+static inline string readToEnd(FILE* f) {
+    long pos = ftell(f);
     fseek(f, 0l, SEEK_END);
     long size = ftell(f);
     fseek(f, 0l, SEEK_SET);
     char* res = malloc(size);
     fread(res, 1, size, f);
-    fclose(f);
     return str(res);
+}
+static inline string readAllText(string path) {
+    FILE* f = fopen(cptr(path), "r");
+    string res = readToEnd(f);
+    fclose(f);
+    return res;
 }
 static inline string runProcess(string command) {
     FILE* f = popen("objdump -d tmp.o", "r");
-    fseek(f, 0l, SEEK_END);
-    long size = ftell(f);
-    fseek(f, 0l, SEEK_SET);
-    char* res = malloc(size);
-    fread(res, 1, size, f);
+    string res = readToEnd(f);
     pclose(f);
-    return str(res);
+    return res;
 }
 static inline void writeAllText(string path, string text) {
     FILE* f = fopen(cptr(path), "w");
