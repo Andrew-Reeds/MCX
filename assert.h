fe76473e452cae3bb3ee327bd0e899d653628093
Coddra <andrew.reeds.mpg@gmail.com>
2022-05-06 17:09:32 +0200

    Stupid me indeed.

--- assert.h
+++ assert.h
@@ -1,6 +1,10 @@
 #ifndef MCX_ASSERT_H
 #define MCX_ASSERT_H
 
-#define assert(condition, message...) if (!(condition)) fprintf(stderr, message)
+#define assert(condition, message...)                       \
+    if (!(condition)) {                                     \
+        fprintf(stderr, message);                           \
+        exit(1);                                            \
+    }
 
 #endif // MCX_ASSERT_H
